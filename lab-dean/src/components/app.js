2f03d88e0130a7c37ebaf7edd3fd9ba60d4cd0e0
Dean <dean.murphy90@gmail.com>
2018-03-12 22:49:19 -0700

    expense items not appearing but functionality almost done

--- lab-dean/src/components/app.js
+++ lab-dean/src/components/app.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import createStore from '../lib/store';
-import Dashboard from './dashboard/dashboard';
-import {BrowserRouter, Route} from 'react-router-dom';
+import Dashboard from './dashboard/index';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const store = createStore();
 
 class App extends React.Component {
   componentDidMount() {
-    store.subscribe(() => console.log('__STATE__:', store.getState()));
+    store.subscribe(() => console.log('__STATE__: ', store.getState()));
   }
 
   render() {
     return (
-      <main className="main-content">
-        <Provider store={store}>
-          <BrowserRouter>
-            <Route exact path="/" component={Dashboard}/>
-          </BrowserRouter>
-        </Provider>
-      </main>
+      <div className="app">
+        <main className="main-content">
+          <Provider store={store}>
+            <BrowserRouter>
+              <Route exact path="/" component={Dashboard} />
+            </BrowserRouter>
+          </Provider>
+        </main>
+      </div>
     );
   }
 }
--- lab-dean/src/main.js
+++ lab-dean/src/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/main.scss';
 import ReactDom from 'react-dom';
 import App from './components/app';
 
